588e82791ea955d702c91efb62afd4faf8abeab9
benjaminCanalesC <135075287+benjaminCanalesC@users.noreply.github.com>
2024-05-16 14:06:02 -0400

    feat: config, eureka and gatway service

---
+++ eureka-service/src/test/java/bcanales/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bcanales.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
